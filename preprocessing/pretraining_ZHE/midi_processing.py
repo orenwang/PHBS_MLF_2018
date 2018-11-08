e81785c0e98666f102000132705f47021a20f156
Zhe Wang <29098798+dragonwdl@users.noreply.github.com>
2018-11-08 20:34:50 +0800

    Update midi_processing.py

--- preprocessing/pretraining_ZHE/midi_processing.py
+++ preprocessing/pretraining_ZHE/midi_processing.py
@@ -1,23 +1,17 @@
-
 # coding: utf-8
 
 # # Import raw data (all midi files) and export pre-test data (single track midi files for guitar, piano or strings)
 
 # ## 1. Import modules and set the path to where the midi files are
 
-# In[9]:
-
 
 from mido import MidiFile
 import os
-path = r'E:\Desktop\MLFproject\MIDI_file\test_select'
+path = r'/Users/dragon/Desktop/Project/MIdi tracks'
 
+print('Numbers counting：')
 
 # ## 2. Scan all files that end with ".mid", and turn them into objects
-
-# In[10]:
-
-
 totalfiles = []
 for path, dirs, files in os.walk(path):
     for filename in files:
@@ -29,81 +23,81 @@ for path, dirs, files in os.walk(path):
             except:
                 continue
 #Show how many midi we got
-len(totalfiles)
+num_total=len(totalfiles)
+print('\t'*2,'Total number of midi files:',num_total)
 
 
-# ## 3. Select those with track name 'guitar' 
-
-# In[11]:
-
-
-selectedtracks = []
+# ## 3. Select those with track name 'guitar'
+selectedtracks_guitar = []
 for i in totalfiles:
     m = 0
     for j, track in enumerate(i.tracks):
         if ('guitar' in track.name) or ('Guitar' in track.name) or ('acoustic guitar' in track.name) or ('Acoustic Guitar' in track.name) or ('Acoustic guitar' in track.name):
-            selectedtracks.append(track)
+            selectedtracks_guitar.append(track)
 #Show how many tracks we got
-len(selectedtracks)
+num_guitar=len(selectedtracks_guitar)
+print('\t'*2,'Number of guitar tracks:',num_guitar)
 
 
-# ## 4. Cut every track to a same length, probably 30 seconds. See: https://mido.readthedocs.io/en/latest/midi_files.html
-
-# ## 5. Put each 30s-track in a new midi file, see also: https://mido.readthedocs.io/en/latest/midi_files.html
-
-# ## 6. Do the same for 'piano'
-
-# ## 7. Do the same for 'strings'
-
-# In[ ]:
-
-
-# #
-# totalfiles = []
-# for path, dirs, files in os.walk(path):
-#     for filename in files:
-#         if (filename.endswith('.MID')) or (filename.endswith('.mid')):
-#             fullpath = os.path.join(path, filename)
-#             try:
-#                 mid = MidiFile(fullpath)
-#                 totalfiles.append(mid)
-#             except:
-#                 continue
-# #Show how many midi we got
-# len(totalfiles)
+# ## 4. Do the same for 'piano'
+selectedtracks_piano = []
+for i in totalfiles:
+    m = 0
+    for j, track in enumerate(i.tracks):
+        if ('piano' in track.name) or ('Piano' in track.name) or ('Left piano' in track.name) or ('Right piano' in track.name) or ('Left Piano' in track.name) or ('Right Piano' in track.name):
+            selectedtracks_piano.append(track)
+#Show how many tracks we got
+num_piano=len(selectedtracks_piano)
+print('\t'*2,'Number of piano tracks:',num_piano)
 
 
-# In[ ]:
+# ## 5. Do the same for 'strings'
+selectedtracks_strings = []
+for i in totalfiles:
+    m = 0
+    for j, track in enumerate(i.tracks):
+        if ('Violin' in track.name) or ('Cuerdas1' in track.name) or ('Cuerdas2' in track.name) or ('Viola' in track.name) or ('Cello' in track.name) or ('Violoncello' in track.name):
+            selectedtracks_strings.append(track)
+#Show how many tracks we got
+num_strings=len(selectedtracks_strings)
+print('\t'*2,'Number of strings tracks:',num_strings)
 
 
-# selectedmidi = []
-# for i in totalfiles:
-#     m = 0
-#     n = 0
-#     for j, track in enumerate(i.tracks):
-#         if (track.name == 'guitar') or (track.name == 'Guitar') or (track.name == 'acoustic guitar') or (track.name == 'Acoustic Guitar') or (track.name == 'Acoustic guitar'):
-#             m = 1
-#         if (track.name == 'voice') or (track.name == 'Voice') or (track.name == 'VOICE') or (track.name == 'vocal') or (track.name == 'Vocal') or (track.name == 'VOCAL') :
-#             n = 1
-#     if (m == 1) and (n == 1):
-#         selectedmidi.append(i)
+# ## 6. Transfer tracks we exact into midi files
+from mido import Message, MidiFile, MidiTrack
+
+mid = MidiFile()
+mid.tracks.append(track)
 
 
-# ## Show messages in the first selected track
+for i in range(0,len(selectedtracks_guitar)):
+    mid = MidiFile()
+    track = selectedtracks_guitar[i]
+    mid.tracks.append(track)
+    mid.save('/Users/dragon/Desktop/Project/New midi/Guitar/new_guitar{}.mid'.format(i))
+    #Show progress
+    print(i if i%5 == 0 else ' ', end = '')
 
-# In[12]:
+for i in range(0,len(selectedtracks_piano)):
+    mid = MidiFile()
+    track = selectedtracks_piano[i]
+    mid.tracks.append(track)
+    mid.save('/Users/dragon/Desktop/Project/New midi/Piano/new_piano{}.mid'.format(i))
+    #Show progress
+    print(i if i%5 == 0 else ' ', end = '')
 
 
-for i in selectedtracks[0:1]:
-    for msg in track:
-        print(msg)
-# print(selectedtracks[0])
+for i in range(0,len(selectedtracks_strings)):
+    mid = MidiFile()
+    track = selectedtracks_strings[i]
+    mid.tracks.append(track)
+    mid.save('/Users/dragon/Desktop/Project/New midi/Strings/new_strings{}.mid'.format(i))
+    #Show progress
+    print(i if i%5 == 0 else ' ', end = '')
 
 
-# In[1]:
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
 
 
-# for i in selectedmidi:
-#     for j, track in enumerate(i.tracks):
-#         print('Track {}: {}'.format(j, track.name))
 
